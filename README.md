a2b9a70f5b63da40f0dfc37017a5c27c2ab37cf1
lessonsharing <maiuri.gaetan@gmail.com>
2018-02-13 17:02:35 +0100

    update README.md & dog.sh

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Dog.sh
 Host Network Running Config Monitoring
 
-
 Dog.sh is a Host Network Moitoring script to check in a glimpse all the infos you need on your running Network configuration.
 To enable a realtime monitoring you have to run the script like this:
 
-user@host:/#watch -n 3 ./dog.sh
-
+```bash
+$ watch -n 3 /bin/bash dog.sh
+```
 
 watch -n 3 : run the script every 3 seconds
--- dog.sh
+++ dog.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
-#run this script with the "watch" command to enable realtime monitoring
-#e.g:"watch -n 3 ./dog.sh" run dog.sh every 3seconds
-echo "dog.sh by bus7d"
+# run this script with the "watch" command to enable realtime monitoring.
+# e.g: "watch -n 3 /bin/bash dog.sh" - run dog.sh every 3 seconds.
+readonly VERSION="{{VERSION}}"
+readonly APPNAME="dog.sh"
+readonly APPDESC="Host Network Moitoring"
+readonly AUTORS="bus7d, lessonsharing"
 
-echo Hello $USER  Dog is on patrol..
+printf "Welcome to %s v%s\n" $APPNAME $VERSION
+printf "%s\n" $APPDESC
+printf "By: %s\n" $AUTORS
+printf "=======================\n"
+
+echo "Hello ${USER}, Dog is on patrol..."
+
 echo "Active Users::" 
-finger
-
+finger || echo "! no finger command found."
 echo ----------------------------------------------------------------
+
 printf "LOCAL IP:: "
 ifconfig |grep -A1 wlan0|grep inet |cut -d "n" -f 2 |cut -d " " -f 2
 echo ----------------------------------------------------------------
-route 
+route || echo "! no route command found."
 echo ----------------------------------------------------------------
 cat /etc/resolv.conf|grep nameserver
 echo ----------------------------------------------------------------
 traceroute -m 3 google.com
 echo ----------------------------------------------------------------
+
 echo "ARP Neighbors"
-arp -a
+arp -a || echo "! no arp command found."
 echo ----------------------------------------------------------------
 iptables -L
 echo ----------------------------------------------------------------
